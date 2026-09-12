8137d4252fe37984c0ba17366d40d9af5a2eb8e7
AlainMateo01 <alainmateo01@gmail.com>
2026-09-12 17:21:50 -0500

    feat: adjust chunk size to get 600 tokens aprox window

--- src/ingestion/splitters.py
+++ src/ingestion/splitters.py
@@ -2,5 +2,5 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document
 
 def chunk_splitter(docs : list[Document]):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=600, chunk_overlap=100)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=2400, chunk_overlap=400)
     return text_splitter.split_documents(docs)
