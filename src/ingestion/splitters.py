a1c94cb631c3723db2b59a769864d425d7a201de
AlainMateo01 <alainmateo01@gmail.com>
2026-09-12 17:20:07 -0500

    feat: add splitter with aprox 660 tokens window with overlaping

---
+++ src/ingestion/splitters.py
@@ -0,0 +1,6 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.documents import Document
+
+def chunk_splitter(docs : list[Document]):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=600, chunk_overlap=100)
+    return text_splitter.split_documents(docs)
