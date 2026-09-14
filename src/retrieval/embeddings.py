5fc8545536e644758dba93695c5b53c2a7beb38c
AlainMateo01 <alainmateo01@gmail.com>
2026-09-13 20:14:11 -0500

    feat: add caching to embedding retrieval function for improved performance

--- src/retrieval/embeddings.py
+++ src/retrieval/embeddings.py
@@ -1,6 +1,9 @@
+from functools import lru_cache
+
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from src.core import GOOGLE_API_KEY
 
+@lru_cache(maxsize=1)
 def get_embedding(model_name: str = "models/gemini-embedding-001", api_key: str = GOOGLE_API_KEY, truncate_length: int = 768):
     """
     Returns the embeddings model to be used for generating embeddings.
